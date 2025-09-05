2103b777f7dd68b0612e451cb74d283965dc270b
starfield <149873042+starfield17@users.noreply.github.com>
2025-09-05 20:02:57 +0800

    Update add_to_sys.sh

--- V1/add_to_sys.sh
+++ V1/add_to_sys.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-
-# 获取当前脚本所在目录
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# 定义软链接的目标路径
 TARGET="/usr/local/bin/ds"
-
-# 检查目标文件是否已经存在
 if [ -f "$TARGET" ]; then
     echo "The script already exists at $TARGET. No changes made."
 else
-    # 创建一个启动脚本来运行当前环境的 python
     echo "#!/bin/bash" | sudo tee "$TARGET" > /dev/null
     echo "python3 $SCRIPT_DIR/Deep_seek_api_rebuild.py" | sudo tee -a "$TARGET" > /dev/null
-
-    # 给软链接和脚本添加执行权限
     sudo chmod +x "$TARGET"
 
     echo "Soft link created at $TARGET"
