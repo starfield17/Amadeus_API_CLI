c47e8f0db7f61e2e390018c137d638b95587cc0b
starfield <149873042+starfield17@users.noreply.github.com>
2025-09-24 11:02:13 +0800

    Update install_requirement.sh

--- V1/install_requirement.sh
+++ V1/install_requirement.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # Install required Python packages using USTC PyPI mirror
-pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple \
-    openai \
-    rich \
-    prompt_toolkit \
-    httpx \
-    httpx-socks \
-    pygments \
-    pyperclip \
-    toml
+pip install -r requirements.txt -i https://mirrors.ustc.edu.cn/pypi/web/simple
 echo "All dependencies installed successfully!"
